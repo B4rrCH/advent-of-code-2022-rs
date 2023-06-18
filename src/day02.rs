e8f22f1505aed7ff1e67837f863eba304c67b663
B4rr <lochaus@hotmail.com>
2023-06-18 16:46:49 +0200

    improvments for day02.rs

--- src/day02.rs
+++ src/day02.rs
@@ -1,30 +1,6 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::str::FromStr;
-
-#[derive(PartialEq, Clone, Copy)]
-enum Move {
-    Rock,
-    Paper,
-    Scissors,
-}
-
-enum Strategy {
-    Answer,
-    Outcome,
-}
-
-impl TryFrom<&String> for Strategy {
-    type Error = ();
-    fn try_from(s: &String) -> Result<Self, Self::Error> {
-        match s.as_str() {
-            "1" => Ok(Self::Answer),
-            "2" => Ok(Self::Outcome),
-            _ => Err(()),
-        }
-    }
-}
 
 pub fn run(args: &[String]) -> std::io::Result<()> {
     let file = File::open("input/day02.txt")?;
@@ -35,28 +11,18 @@ pub fn run(args: &[String]) -> std::io::Result<()> {
         .and_then(|s| s.try_into().ok())
         .unwrap_or(Strategy::Answer);
 
-    let score: i32 = reader
+    let score: usize = reader
         .lines()
         .map_while(Result::ok)
         .filter_map(|s| {
             let mut chars = s.chars();
-            let opponent = chars.next().and_then(|c| match c {
-                'A' => Some(Move::Rock),
-                'B' => Some(Move::Paper),
-                'C' => Some(Move::Scissors),
-                _ => None,
-            });
+            let opponent: Option<Move> = chars.next().and_then(|c| c.try_into().ok());
 
             // Skip the space
             _ = chars.next();
 
-            let player = chars.next().and_then(|c| match strategy {
-                Strategy::Answer => match c {
-                    'X' => Some(Move::Rock),
-                    'Y' => Some(Move::Paper),
-                    'Z' => Some(Move::Scissors),
-                    _ => None,
-                },
+            let player: Option<Move> = chars.next().and_then(|c| match strategy {
+                Strategy::Answer => c.try_into().ok(),
                 Strategy::Outcome => match c {
                     // player loses
                     'X' => match opponent {
@@ -85,26 +51,61 @@ pub fn run(args: &[String]) -> std::io::Result<()> {
                 _ => None,
             }
         })
-        .map(|game| {
-            let (opponent, player) = game;
-
-            let shape_score = match player {
-                Move::Rock => 1,
-                Move::Paper => 2,
-                Move::Scissors => 3,
-            };
-
-            let win_score = match (player, opponent) {
-                (x, y) if x == y => 3,
-                (Move::Rock, Move::Scissors)
-                | (Move::Scissors, Move::Paper)
-                | (Move::Paper, Move::Rock) => 6,
-                _ => 0,
-            };
-            shape_score + win_score
-        })
+        .map(get_player_score_for_game)
         .sum();
 
     println!("Scored: {}", score);
     Ok(())
+}
+
+#[derive(PartialEq, Clone, Copy)]
+enum Move {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+enum Strategy {
+    Answer,
+    Outcome,
+}
+
+impl TryFrom<&String> for Strategy {
+    type Error = ();
+    fn try_from(s: &String) -> Result<Self, Self::Error> {
+        match s.as_str() {
+            "1" => Ok(Self::Answer),
+            "2" => Ok(Self::Outcome),
+            _ => Err(()),
+        }
+    }
+}
+
+impl TryFrom<char> for Move {
+    type Error = ();
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        match c {
+            'A' | 'X' => Ok(Move::Rock),
+            'B' | 'Y' => Ok(Move::Paper),
+            'C' | 'Z' => Ok(Move::Scissors),
+            _ => Err(()),
+        }
+    }
+}
+
+fn get_player_score_for_game(game: (Move, Move)) -> usize {
+    let (opponent, player) = game;
+    let shape_score = match player {
+        Move::Rock => 1,
+        Move::Paper => 2,
+        Move::Scissors => 3,
+    };
+    let win_score = match (player, opponent) {
+        (x, y) if x == y => 3,
+        (Move::Rock, Move::Scissors)
+        | (Move::Scissors, Move::Paper)
+        | (Move::Paper, Move::Rock) => 6,
+        _ => 0,
+    };
+    shape_score + win_score
 }
