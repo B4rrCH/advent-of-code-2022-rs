85d9443b12874fdf0acee53439c41c2e4fae591a
B4rr <lochaus@hotmail.com>
2023-06-18 17:40:32 +0200

    directly read file as bytes instead of String

--- src/day06.rs
+++ src/day06.rs
@@ -1,27 +1,26 @@
 use std::collections::HashSet;
-use std::fs::read_to_string;
+use std::fs::File;
+use std::io::Read;
 
 pub fn run(args: &[String]) -> std::io::Result<()> {
-    let input: String = read_to_string("input/day06.txt")?;
+    let mut file = File::open("foo.txt")?;
+    let mut input = vec![];
+    file.read_to_end(&mut input)?;
 
     match args.get(0).map(|s| s.parse::<i32>()) {
-        Some(Ok(1)) => find_start_of_distinct(input, 4),
-        Some(Ok(2)) => find_start_of_distinct(input, 14),
+        Some(Ok(1)) => find_start_of_distinct(&input, 4),
+        Some(Ok(2)) => find_start_of_distinct(&input, 14),
         _ => panic!("Unknown part"),
     }?;
 
     Ok(())
 }
 
-fn find_start_of_distinct(input: String, nr_of_distinct: usize) -> std::io::Result<()> {
-    if let Some((index, _)) =
-        input
-            .as_bytes()
-            .windows(nr_of_distinct)
-            .enumerate()
-            .find(|(_, bytes)| {
-                (*bytes).iter().copied().collect::<HashSet<u8>>().len() == nr_of_distinct
-            })
+fn find_start_of_distinct(input: &[u8], nr_of_distinct: usize) -> std::io::Result<()> {
+    if let Some((index, _)) = input
+        .windows(nr_of_distinct)
+        .enumerate()
+        .find(|(_, bytes)| (*bytes).iter().copied().collect::<HashSet<_>>().len() == nr_of_distinct)
     {
         println!(
             "First {} different characters start at index {}",
