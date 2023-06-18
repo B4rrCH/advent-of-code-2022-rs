490cca78ac1ac8977eaf99114d89e48fa4cc0b15
B4rr <lochaus@hotmail.com>
2023-06-18 17:32:20 +0200

    solution for day06

--- src/day06.rs
+++ src/day06.rs
@@ -1,28 +1,33 @@
-use std::fs::File;
-use std::io::BufReader;
+use std::collections::HashSet;
+use std::fs::read_to_string;
 
 pub fn run(args: &[String]) -> std::io::Result<()> {
-    let file = File::open("input/day06.txt")?;
-    let reader = BufReader::new(file);
+    let input: String = read_to_string("input/day06.txt")?;
 
     match args.get(0).map(|s| s.parse::<i32>()) {
-        Some(Ok(1)) => run_part1(reader),
-        Some(Ok(2)) => run_part2(reader),
-        _ => {
-            println!("Unknown part");
-            Ok(())
-        }
+        Some(Ok(1)) => find_start_of_distinct(input, 4),
+        Some(Ok(2)) => find_start_of_distinct(input, 14),
+        _ => panic!("Unknown part"),
     }?;
 
     Ok(())
 }
 
-fn run_part1(reader: BufReader<File>) -> std::io::Result<()> {
-    _ = reader;
-    Ok(())
-}
-
-fn run_part2(reader: BufReader<File>) -> std::io::Result<()> {
-    _ = reader;
+fn find_start_of_distinct(input: String, nr_of_distinct: usize) -> std::io::Result<()> {
+    if let Some((index, _)) =
+        input
+            .as_bytes()
+            .windows(nr_of_distinct)
+            .enumerate()
+            .find(|(_, bytes)| {
+                (*bytes).iter().copied().collect::<HashSet<u8>>().len() == nr_of_distinct
+            })
+    {
+        println!(
+            "First {} different characters start at index {}",
+            nr_of_distinct,
+            index + nr_of_distinct
+        );
+    }
     Ok(())
 }
