5a7b654d29ee6d397fe2d25c698c82627a2098c4
B4rr <lochaus@hotmail.com>
2023-06-18 15:59:20 +0200

    inlin everything in day02.rs

--- src/day02.rs
+++ src/day02.rs
@@ -15,102 +15,6 @@ enum Strategy {
     Outcome,
 }
 
-trait Rps {
-    fn get_better(worse_move: Move) -> Move;
-    fn get_worse(better_move: Move) -> Move;
-}
-
-impl Rps for Move {
-    fn get_better(worse_move: Move) -> Move {
-        match worse_move {
-            Move::Paper => Move::Scissors,
-            Move::Rock => Move::Paper,
-            Move::Scissors => Move::Rock,
-        }
-    }
-
-    fn get_worse(better_move: Move) -> Move {
-        match better_move {
-            Move::Scissors => Move::Paper,
-            Move::Paper => Move::Rock,
-            Move::Rock => Move::Scissors,
-        }
-    }
-}
-
-impl std::cmp::PartialOrd<Move> for Move {
-    fn partial_cmp(&self, other: &Move) -> Option<std::cmp::Ordering> {
-        if *self == *other {
-            Some(std::cmp::Ordering::Equal)
-        } else {
-            Some(match (*self, *other) {
-                (Move::Rock, Move::Scissors) => std::cmp::Ordering::Greater,
-                (Move::Scissors, Move::Paper) => std::cmp::Ordering::Greater,
-                (Move::Paper, Move::Rock) => std::cmp::Ordering::Greater,
-                _ => std::cmp::Ordering::Less,
-            })
-        }
-    }
-}
-
-fn parse_answer(c: char) -> Option<Move> {
-    match c {
-        'X' => Some(Move::Rock),
-        'Y' => Some(Move::Paper),
-        'Z' => Some(Move::Scissors),
-        _ => None,
-    }
-}
-
-fn get_outcome(c: char, opponent: Option<Move>) -> Option<Move> {
-    match c {
-        'X' => opponent.map(Move::get_worse),
-        'Y' => opponent,
-        'Z' => opponent.map(Move::get_better),
-        _ => None,
-    }
-}
-
-fn parse(s: String, strategy: Strategy) -> Option<(Move, Move)> {
-    let mut chars = s.chars();
-    let opponent = match chars.next() {
-        Some('A') => Some(Move::Rock),
-        Some('B') => Some(Move::Paper),
-        Some('C') => Some(Move::Scissors),
-        _ => None,
-    };
-
-    // Skip the space
-    _ = chars.next();
-
-    let player = match strategy {
-        Strategy::Answer => chars.next().and_then(parse_answer),
-        Strategy::Outcome => chars.next().and_then(|c| get_outcome(c, opponent)),
-    };
-
-    match (opponent, player) {
-        (Some(x), Some(y)) => Some((x, y)),
-        _ => None,
-    }
-}
-
-fn points(game: (Move, Move)) -> i32 {
-    let (opponent, player) = game;
-
-    let shape_score = match player {
-        Move::Rock => 1,
-        Move::Paper => 2,
-        Move::Scissors => 3,
-    };
-
-    let win_score = match player.partial_cmp(&opponent) {
-        Some(std::cmp::Ordering::Greater) => 6,
-        Some(std::cmp::Ordering::Equal) => 3,
-        _ => 0,
-    };
-    shape_score + win_score
-}
-
 pub fn run(args: &[String]) -> std::io::Result<()> {
     let file = File::open("input/day02.txt")?;
     let reader = BufReader::new(file);
@@ -120,11 +24,114 @@ pub fn run(args: &[String]) -> std::io::Result<()> {
         _ => Strategy::Outcome,
     };
 
-    let score = reader
-        .lines()
-        .map_while(Result::ok)
-        .filter_map(|s| parse(s, strategy))
-        .fold(0, |p, g| p + points(g));
+    let score: i32 = match strategy {
+        Strategy::Answer => {
+            reader
+                .lines()
+                .map_while(Result::ok)
+                .filter_map(|s| {
+                    let mut chars = s.chars();
+                    let opponent = chars.next().and_then(|c| match c {
+                        'A' => Some(Move::Rock),
+                        'B' => Some(Move::Paper),
+                        'C' => Some(Move::Scissors),
+                        _ => None,
+                    });
+
+                    // Skip the space
+                    _ = chars.next();
+
+                    let player = chars.next().and_then(|c| match c {
+                        'X' => Some(Move::Rock),
+                        'Y' => Some(Move::Paper),
+                        'Z' => Some(Move::Scissors),
+                        _ => None,
+                    });
+
+                    match (opponent, player) {
+                        (Some(x), Some(y)) => Some((x, y)),
+                        _ => None,
+                    }
+                })
+                .map(|game| {
+                    let (opponent, player) = game;
+
+                    let shape_score = match player {
+                        Move::Rock => 1,
+                        Move::Paper => 2,
+                        Move::Scissors => 3,
+                    };
+
+                    let win_score = match (player, opponent) {
+                        (x, y) if x == y => 3,
+                        (Move::Rock, Move::Scissors)
+                        | (Move::Scissors, Move::Paper)
+                        | (Move::Paper, Move::Rock) => 6,
+                        _ => 0,
+                    };
+                    shape_score + win_score
+                })
+                .sum()
+        }
+        Strategy::Outcome => {
+            reader
+                .lines()
+                .map_while(Result::ok)
+                .filter_map(|s| {
+                    let mut chars = s.chars();
+                    let opponent = chars.next().and_then(|c| match c {
+                        'A' => Some(Move::Rock),
+                        'B' => Some(Move::Paper),
+                        'C' => Some(Move::Scissors),
+                        _ => None,
+                    });
+
+                    // Skip the space
+                    _ = chars.next();
+
+                    let player = chars.next().and_then(|c| match c {
+                        'X' => match opponent {
+                            Some(Move::Scissors) => Some(Move::Paper),
+                            Some(Move::Paper) => Some(Move::Rock),
+                            Some(Move::Rock) => Some(Move::Scissors),
+                            _ => None,
+                        },
+                        'Y' => opponent,
+                        'Z' => match opponent {
+                            Some(Move::Paper) => Some(Move::Scissors),
+                            Some(Move::Rock) => Some(Move::Paper),
+                            Some(Move::Scissors) => Some(Move::Rock),
+                            _ => None,
+                        },
+                        _ => None,
+                    });
+
+                    match (opponent, player) {
+                        (Some(x), Some(y)) => Some((x, y)),
+                        _ => None,
+                    }
+                })
+                .map(|game| {
+                    let (opponent, player) = game;
+
+                    let shape_score = match player {
+                        Move::Rock => 1,
+                        Move::Paper => 2,
+                        Move::Scissors => 3,
+                    };
+
+                    let win_score = match (player, opponent) {
+                        (x, y) if x == y => 3,
+                        (Move::Rock, Move::Scissors)
+                        | (Move::Scissors, Move::Paper)
+                        | (Move::Paper, Move::Rock) => 6,
+                        _ => 0,
+                    };
+                    shape_score + win_score
+                })
+                .sum()
+        }
+    };
 
     println!("Scored: {}", score);
     Ok(())
